17398552011ab327fc082d46a9751df0fdf65c6f
Daniel Greco <daniel.greco@fdacs.gov>
2024-05-28 12:45:42 -0400

    Add Genre

---
+++ Bookstore.App/Migrations/202405281638148_initial.cs
@@ -0,0 +1,51 @@
+﻿namespace Bookstore.App.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Books",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                        Description = c.String(),
+                        Author = c.String(),
+                        IsActive = c.Boolean(nullable: false),
+                        CreatedBy = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        LastUpdatedBy = c.String(),
+                        ModificationDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                        Email = c.String(),
+                        DateOfBirth = c.DateTime(),
+                        IsActive = c.Boolean(nullable: false),
+                        CreatedBy = c.String(),
+                        CreationDate = c.DateTime(nullable: false),
+                        LastUpdatedBy = c.String(),
+                        ModificationDate = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Customers");
+            DropTable("dbo.Books");
+        }
+    }
+}
---
+++ Bookstore.App/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookstore.App.Models
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public DateTime ModificationDate { get; set; }
+    }
+}
