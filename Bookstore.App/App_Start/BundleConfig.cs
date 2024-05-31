50db9813933989726f9fdf8bf4f703afb9e63cb7
Daniel Greco <daniel.greco@fdacs.gov>
2024-05-31 11:03:07 -0400

    Added new CSS file.

--- Bookstore.App/App_Start/BundleConfig.cs
+++ Bookstore.App/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace Bookstore.App
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/sandstone-bootstrap.css",
                       "~/Content/site.css"));
         }
     }
