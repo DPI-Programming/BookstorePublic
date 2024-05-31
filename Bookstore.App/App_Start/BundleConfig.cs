b4e88dc7b5f62b13a5d3fa84ad985252dfd5bd4a
Daniel Greco <daniel.greco@fdacs.gov>
2024-05-31 11:13:48 -0400

    Added in date picker

--- Bookstore.App/App_Start/BundleConfig.cs
+++ Bookstore.App/App_Start/BundleConfig.cs
@@ -9,7 +9,9 @@ namespace Bookstore.App
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery-ui-1.13.2.js",
+                        "~/Scripts/datepicker.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -23,6 +25,7 @@ namespace Bookstore.App
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                       "~/Content/themes/base/jquery-ui.css",
                       "~/Content/bootstrap.css",
                       "~/Content/sandstone-bootstrap.css",
                       "~/Content/site.css"));
